0e9c67ca81cd706f0dcbe1769fe669705c7130cb
Edson Sousa <edson3711.es@gmail.com>
2024-09-05 21:02:13 -0300

    Adiciona .gitignore para ignorar arquivos desnecessários

---
+++ src/test/java/com/edson/AgendaTelefonicaAPI/AgendaTelefonicaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edson.AgendaTelefonicaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaTelefonicaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
